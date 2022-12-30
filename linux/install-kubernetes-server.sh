85af41cd0286e40885cc916a988b5866ee36e171
Jacinto García <jacinto.garcialopez@everis.nttdata.com>
2022-12-30 14:00:27 +0100

    Install Ubuntu Server Tools

---
+++ linux/install-kubernetes-server.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+sudo apt update
+sudo apt upgrade
+# Clone courses repository
+git clone https://github.com/jacintogl82/courses.git
+
+# Install VSFTP
+sudo apt install -y vsftpd
+sudo cp courses/linux/ftp/vsftpd.conf /etc/vsftpd.conf
+sudo ufw allow 20,21,990/tcp
+sudo systemctl restart vsftpd
+sudo systemctl status vsftpd
+
+# Install Docker
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo docker run hello-world
+sudo usermod -aG docker $USER && newgrp docker
+
+# Install Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube start
+minikube docker-env && eval $(minikube -p minikube docker-env)
+minikube dashboard
+minikube addons enable ingress
+minikube addons enable istio
+minikube addons enable istio-provisioner
+
+# Install Kubectl
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# Install Java JDK
+sudo apt-get install -y openjdk-8-jdk
+sudo apt-get install -y openjdk-11-jdk
+sudo apt install -y default-jdk
+java -version
+# To seletc an specific java version execute
+# sudo update-alternatives --set java path
+
+# Install Maven
+sudo apt install -y maven	
+mvn -version
+
+# Install NodeJS
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
+nvm install v18.12.1
